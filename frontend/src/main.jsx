8bc14e6831a8734f98e28439474d4103087d23a7
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-02-03 22:58:00 +0530

    fix: merge conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Canvas from './components/ui/Canvas.jsx';
+import { Routes } from 'react-router-dom';
+import Navbar from './components/ui/NavBar.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    <Router>
-      
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/canvas" element={<Canvas />} />
-      </Routes>
-    </Router>
+    <Navbar/>
+    <App/>
   </StrictMode>
 );
 
