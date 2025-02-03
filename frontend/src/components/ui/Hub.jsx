674e41dff3fe5e9c2838241d6b008911827fddbd
Pranesh-1-2-3 <pranesh.pmuthukumar2022@vitstudent.ac.in>
2025-02-03 23:39:39 +0530

    Routing

--- frontend/src/components/ui/Hub.jsx
+++ frontend/src/components/ui/Hub.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Navbar from './NavBar.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const Hub = () => {
-  const handleLeftClick = () => {
-    alert('Left section clicked');
-  };
+    const navigate = useNavigate();
+
+    const handleLeftClick = () => {
+      navigate('/canvas');
+    };
 
   const handleRightTopClick = () => {
-    alert('Top-right section clicked');
+    alert('Collab clicked');
   };
 
   const handleRightBottomClick = () => {
-    alert('Bottom-right section clicked');
+    alert('My notes clicked');
   };
 
   return (
@@ -24,7 +27,7 @@ const Hub = () => {
 
         <div className="w-full h-1/2 flex flex-col justify-center items-center pl-0 p-4">
         <motion.h1
-  className="text-6xl font-inter text-white text-center relative z-10 mb-4 mt-25"
+  className="text-6xl font-bold font-inter text-white text-center relative z-10 mb-4 mt-25"
   initial={{ clipPath: 'inset(0 100% 0 0)' }}
   animate={{ clipPath: 'inset(0 0% 0 0)' }}
   transition={{ duration: 2, ease: "easeOut" }}
