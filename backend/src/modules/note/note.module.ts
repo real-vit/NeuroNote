77ef6a47296564777b03443dc8d377a0ed62b1e3
GAUTHAM09012004 <gautham.s2022@vitstudent.ac.in>
2025-02-05 02:25:44 +0530

    Integration with ML

--- backend/src/modules/note/note.module.ts
+++ backend/src/modules/note/note.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { NoteController } from './note.controller';
 import { NoteService } from './note.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { NoteController } from './note.controller';  // Import the controller
+import { PrismaService } from '../../prisma/prisma.service';
 import { RoomService } from '../room/room.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  controllers: [NoteController],
-  providers: [NoteService,PrismaService,RoomService]
+  imports: [
+    HttpModule.register({ timeout: 5000, maxRedirects: 5 }),
+  ],
+  controllers: [NoteController],  // Ensure the controller is listed here
+  providers: [NoteService, PrismaService, RoomService],
+  exports: [NoteService],
 })
 export class NoteModule {}
+
